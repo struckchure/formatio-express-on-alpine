b24560e01acecd38b58986d6a1fafbd021fb4564
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-10-30 12:14:37 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (_req, res) => {
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  res.json({
+    app: "expressjs",
+    datetime: formattedDate,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`INFO: app listening on port ${port}`);
+});
